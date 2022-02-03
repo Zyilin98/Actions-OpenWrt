b6309a96431ab61b53c92ea48c92a28842d1de09
Unmser_euces <61766598+Zyilin98@users.noreply.github.com>
2022-02-03 10:05:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,20 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-#sirpdboy
-#git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
-
-git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 
 #修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
-
 git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
-git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 git clone https://github.com/rufengsuixing/luci-app-zerotier.git package/luci-app-zerotier
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
+
